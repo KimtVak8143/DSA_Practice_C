56b6255fd59ad68588352f84c110cc51abca6ffa
KimtVak8143 <mk8143@srmist.edu.in>
2021-08-22 19:12:24 +0530

    q1

--- Solutions/Q1.c
+++ Solutions/Q1.c
@@ -1,13 +1,30 @@
 //
 // Created by mohit on 22-08-2021.
 //
-
+#include<math.h>
 #include<stdio.h>
+int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
+    int sum=0;
+    int s1,s2,i,j;
+    s1=s2=0;
+    for(i=0;i<arr_rows;i++)
+    {   //for(j=0;j<arr_columns;j++)
+        s1+=arr[i][i];
+        s2+=arr[i][arr_rows-i-1];
+    }
+    sum=abs(s1-s2);
+    return sum;
+}
+
 int main()
-{   printf("Hello,\n");
-    char name[20];
-    printf("Enter your name : \t");
-    scanf("%s",name);
-    printf("\nHello %s",name);
+{   int r,c;
+    printf("Enter order of matrix :\t");
+    scanf("%d",&r);
+    c = r;
+    int arr[r][c];
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d",&arr[i][j]);
+    printf("%d",diagonalDifference(r,c,arr));
     return 0;
 };
