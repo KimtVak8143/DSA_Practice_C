19bfac56d7c9b106026ac785023a5fb36f2d5701
KimtVak8143 <mk8143@srmist.edu.in>
2021-08-23 19:06:04 +0530

    new changes

--- SolREADME.md
+++ SolREADME.md
@@ -1,7 +1,7 @@
 # DSA_practice
 Collections of questions solved under DSA
 Language : C/C++
-1. Problem 1 :
+1. Diagonal Difference
 2.  
 3.  
 4. 
--- Solutions/Q1.c
+++ Solutions/Q1.c
@@ -1,30 +1,33 @@
-//
-// Created by mohit on 22-08-2021.
-//
 #include<math.h>
 #include<stdio.h>
-int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
-    int sum=0;
-    int s1,s2,i,j;
-    s1=s2=0;
-    for(i=0;i<arr_rows;i++)
-    {   //for(j=0;j<arr_columns;j++)
-        s1+=arr[i][i];
-        s2+=arr[i][arr_rows-i-1];
-    }
-    sum=abs(s1-s2);
-    return sum;
-}
-
+#include<stdlib.h>
+int i,j;
 int main()
-{   int r,c;
+{   int row,col;
     printf("Enter order of matrix :\t");
-    scanf("%d",&r);
-    c = r;
-    int arr[r][c];
-    for(int i=0;i<r;i++)
-        for(int j=0;j<c;j++)
+    scanf("%d",&row);
+    col = row;
+    int arr[row][row];
+    for(i=0;i<row;i++)
+    {   printf("Row %d\n",i+1);
+        for(j=0;j<col;j++)
+        {   printf("Enter the %d element :\n",j+1);
             scanf("%d",&arr[i][j]);
-    printf("%d",diagonalDifference(r,c,arr));
+        }
+    }
+    int s1=0,s2=0;
+    for(i=0;i<row;i++)
+    {   for(j=0;j<col;j++)
+    {   if(i==j)
+            s1+=arr[i][j];
+        if(i==row-j-1)
+            s2+=arr[i][j];
+    }
+    }
+    printf("Sum1 %d\n",s1);
+    printf("Sum2 %d\n",s2);
+    int sum=abs(s1-s2);
+    printf("Sum Abs : %d",sum);
+    //printf("%d",diagonalDifference(row,col,arr));
     return 0;
-};
+}
